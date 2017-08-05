57769dbea689d8b297befee6444c88a0b2210892
donkeizluv <donkeizluv@gmail.com>
2017-08-05 08:51:01 +0700

    initial commit

---
+++ HelpdeskKit/HelpdeskKit/Rules/NotEmptyVadidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace HelpdeskKit.Rules
+{
+    public class VadidateSearchString : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string input = (value ?? "").ToString().Trim();
+            if (string.IsNullOrEmpty(input)) return ValidationResult.ValidResult;
+            //check for invalid char in input
+            if (true)
+            {
+                return new ValidationResult(true, "User found");
+            }
+            else
+            {
+                return new ValidationResult(false, "User not found");
+            }
+        }
+    }
+}
