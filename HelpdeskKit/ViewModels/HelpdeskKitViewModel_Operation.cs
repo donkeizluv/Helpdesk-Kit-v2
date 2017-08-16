bfb60353cab7ddbd0c000a5918e8a40d24881d09
donkeizluv <donkeizluv@gmail.com>
2017-08-16 21:09:12 +0700

    added snackbar

--- HelpdeskKit/ViewModels/HelpdeskKitViewModel_Operation.cs
+++ HelpdeskKit/ViewModels/HelpdeskKitViewModel_Operation.cs
@@ -60,27 +60,52 @@ namespace HelpdeskKit.ViewModels
             }
         }
 
-        public RelayCommand UnlockCommand => new RelayCommand(UnlockMethod, o=>(o != null));
+        public RelayCommand UnlockCommand => 
+            new RelayCommand(UnlockMethod, o=>(o != null));
         private void UnlockMethod(object param)
         {
-            if (param == null) throw new ArgumentNullException();
-            if (param.GetType() != typeof(User)) throw new ArgumentException();
+            if (param == null)
+                throw new ArgumentNullException();
+            if (param.GetType() != typeof(User))
+                throw new ArgumentException();
             _ad.Unlock(param as User);
             OnPropertyChanged(nameof(CurrentUser));
+            DisplaySnackBar("Account unlocked");
         }
 
-        public RelayCommand ChangePasswordCommand => new RelayCommand(o=>ShowInputDialog("Password", false, ContinueChangePasswordMethod), o=>(o != null));
+        public RelayCommand ChangeBatchCommand =>
+                new RelayCommand(o => ShowInputDialog("Batch", false,
+                ContinueChangeBatchMethod), o => (o != null));
         //public RelayCommand ChangeBatchCommand => new RelayCommand((o) => ShowInputDialog("Batch", ContinueChangePasswordMethod), (o) => (o != null));
-
-        private void ContinueChangePasswordMethod(object context)
+        private void ContinueChangeBatchMethod(object context)
         {
-            if (context.GetType() != typeof(InputDialogViewModel)) throw new InvalidOperationException();
+            if (context.GetType() != typeof(InputDialogViewModel))
+                throw new InvalidOperationException();
             var vm = (InputDialogViewModel)context;
             if (vm.Result)
             {
-                if(string.IsNullOrEmpty(vm.Input)) throw  new InvalidProgramException();
+                if (string.IsNullOrEmpty(vm.Input))
+                    throw new InvalidProgramException();
+                _ad.ChangeBatch(CurrentUser, vm.Input);
+                OnPropertyChanged(nameof(CurrentUser));
+                DisplaySnackBar("Batch changed");
+            }
+        }
+        public RelayCommand ChangePasswordCommand =>
+                new RelayCommand(o => ShowInputDialog("Password", false,
+                ContinueChangePasswordMethod), o => (o != null));
+        private void ContinueChangePasswordMethod(object context)
+        {
+            if (context.GetType() != typeof(InputDialogViewModel))
+                throw new InvalidOperationException();
+            var vm = (InputDialogViewModel)context;
+            if (vm.Result)
+            {
+                if(string.IsNullOrEmpty(vm.Input))
+                    throw  new InvalidProgramException();
                 _ad.ChangePassword(CurrentUser, vm.Input);
                 OnPropertyChanged(nameof(CurrentUser));
+                DisplaySnackBar("Password changed");
             }
         }
         private void ShowInputDialog(string inputLabel, bool allowEmptyInput, Action<object> continueWith)
